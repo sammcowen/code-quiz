916f5ba37c2ed6d401d1e164f741b9cee9081b1a
HBK <sammcowen@gmail.com>
2022-01-14 00:18:32 -0800

    questions appear after answer is submitted

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var answer1 = document.getElementById('answer1');
 var answer2= document.getElementById('answer2');
 var answer3= document.getElementById('answer3');
 var answer4= document.getElementById('answer4');
-//var answers = document.getElementById('.para');
+
 // variables 
 var startBtn = document.querySelector('#start');
 let currentIndex = 0; 
@@ -13,7 +13,7 @@ let currentIndex = 0;
 var questions = [
     {
         "questionNum": 1,
-        "questionText":"1.Which of the following functions of Array object extracts a section of an array and returns a new array?",
+        "questionText":"Which of the following functions of Array object extracts a section of an array and returns a new array?",
         "answerOptions":['a.splice()',
             'b.slice()',
             'c.push()',
@@ -22,44 +22,44 @@ var questions = [
     },
       {
       "questionNum": 2,
-      "questionText": "2.Which is a type of pop up box in Javascript?",
+      "questionText": "Which is a type of pop up box in Javascript?",
       "answerOptions": ['a. prompt',
       'b. alert',
              'c. confirm',
              'd. all of the above'],
          "correctAnswer": "d. all of the above"
      },
-    //  {
-    //    "questionNum": 3,
-    //    "questionText": "3.Which data type returns a value of true or false?",
-    //    "answerOptions": ['a. Boolean',
-        //    'b. string',
-        //    'c. number', 
-        //    'd. null'],
-    //    "correctAnswer": "a. Boolean"
-//    },
-//    {
-    //    "questionNum": 4,
-    // "questionText": "4. How do you call/invoke a function named myFunction?",
-//    "answerOptions": ['a. myFunction = function',
-    //    'b. myFunction();',
-    //    'c. var myFunction= function();' ,
-    //    'd. none of the above'],
-//    "correctAnswer": "b. myFunction();"
+     {
+       "questionNum": 3,
+       "questionText": "Which data type returns a value of true or false?",
+       "answerOptions": ['a. Boolean',
+           'b. string',
+           'c. number', 
+           'd. null'],
+       "correctAnswer": "a. Boolean"
+   },
+    {
+       "questionNum": 4,
+    "questionText": "How do you call/invoke a function named myFunction?",
+   "answerOptions": ['a. myFunction = function',
+       'b. myFunction();',
+       'c. var myFunction= function();' ,
+       'd. none of the above'],
+   "correctAnswer": "b. myFunction();"
 
-//    },
-//    {
-    //    "questionNum": 5,
-    //    "questionText":"5. Which of the following is the proper syntax for an Array?",
-    //    "answerOptions":[
-        //    'a. var fruits = ["apples","bananas", "oranges"];',
-        //    'b. var fruits = {apples, bananas, oranges};',
-        //    'c. var fruits = (1: apples, 2: bananas, 3: oranges);',
-        //    'd. none of the above'],
-        //    "correctAnswer": 'a. var fruits = ["apples","bananas", "oranges"];'
-    //    
+   },
+   {
+       "questionNum": 5,
+       "questionText":"Which of the following is the proper syntax for an Array?",
+       "answerOptions":[
+           'a. var fruits = ["apples","bananas", "oranges"];',
+           'b. var fruits = {apples, bananas, oranges};',
+           'c. var fruits = (1: apples, 2: bananas, 3: oranges);',
+           'd. none of the above'],
+           "correctAnswer": 'a. var fruits = ["apples","bananas", "oranges"];'
+       
 
-    //  }
+     }
 
 ];
 
@@ -67,9 +67,14 @@ var questions = [
 
 //INTIATORS
 startBtn.addEventListener('click', startQuiz);
-answers.addEventListener('click', nextQuestion);
+answer1.addEventListener('click', nextQuestion);
+answer2.addEventListener('click', nextQuestion);
+answer3.addEventListener('click', nextQuestion);
+answer4.addEventListener('click', nextQuestion);
 
 function startQuiz() {
+
+
     var timeLeft = 60;
     
     var gameTime = setInterval(function() {
@@ -84,21 +89,36 @@ function startQuiz() {
             clearInterval(gameTime);
         }
     }, 1000);
+    answer1.style.display = "block";
+    answer2.style.display = "block";
+    answer3.style.display = "block";
+    answer4.style.display = "block";
     textEl.textContent = questions[currentIndex].questionText ;
     answer1.textContent = questions[currentIndex].answerOptions[0]; 
     answer2.textContent = questions[currentIndex].answerOptions[1];
     answer3.textContent = questions[currentIndex].answerOptions[2];
     answer4.textContent = questions[currentIndex].answerOptions[3];  
 };
-// ** THIS IS THE FUNCTION I TRIED TO CREATE TO GET THE NEXT QUESTION 
-// TO APPEAR BUT IT WASNT WORKING.
+// this is how the next questions show up.
 function nextQuestion() {
     
+    currentIndex ++;
     textEl.textContent = questions[currentIndex].questionText ;
     answer1.textContent = questions[currentIndex].answerOptions[0]; 
     answer2.textContent = questions[currentIndex].answerOptions[1];
     answer3.textContent = questions[currentIndex].answerOptions[2];
     answer4.textContent = questions[currentIndex].answerOptions[3]; 
-    currentIndex ++;
-    };
+    
+};
+// trying to get the game end to display if all questions are 
+// clicked or the timer is up.and remove the buttons
+function endGame() {
+    if (currentIndex > 4 || timeLeft === 0) {
+        textEl.textContent = "Game's over!";
+        answer1.style.display="none";
+        answer2.style.display="none";
+        answer3.style.display="none";
+        answer4.style.display="none";
 
+    }
+}
